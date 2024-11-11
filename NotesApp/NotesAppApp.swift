6bfa26d7f6f114bcb227b6f8e64a2920adc5e706
Adarsh Shukla <adarshshukla10012002@gmail.com>
2024-11-11 22:18:13 +0530

    Initial Commit

---
+++ NotesApp/NotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAppApp.swift
+//  NotesApp
+//
+//  Created by adarsh shukla on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct NotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NotesAppTests/NotesAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  NotesAppTests.swift
+//  NotesAppTests
+//
+//  Created by adarsh shukla on 11/11/24.
+//
+
+import Testing
+
+struct NotesAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
